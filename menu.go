b50f71e0eec62481a4134a7a367caa775af23e8d
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-08-07 02:39:52 +0800

    add sort for menu

--- menu.go
+++ menu.go
@@ -1,5 +1,7 @@
 package auth
 
+import "sort"
+
 // MenuTree 主菜单sidebar 列表
 type MenuTree struct {
 	// 主菜单名称
@@ -13,4 +15,16 @@ type MyAccessData struct {
 	ID         string     `json:"id"`
 	PathsAllow []string   `json:"paths_allow"`
 	MenuTree   []MenuTree `json:"menu_tree"`
+}
+
+// ByName implements sort.Interface for []MenuTree based on the Name field.
+type ByName []MenuTree
+
+func (a ByName) Len() int           { return len(a) }
+func (a ByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByName) Less(i, j int) bool { return a[i].Name < a[j].Name }
+
+// SortMenuTree sorts the MenuTree slice by Name.
+func (m *MyAccessData) SortMenuTree() {
+	sort.Sort(ByName(m.MenuTree))
 }
